a3dc7dc03941dc0267c61f1ec09dd0b0ebf32c3c
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-06-03 11:23:12 -0300

    Gato

--- Guias/Meta/objetivo.py
+++ Guias/Meta/objetivo.py
@@ -1,7 +1,3 @@
-import pyglet
-pyglet.options['shadow_window'] = False
-pyglet.window.Window(visible=False)
-
 import pygame
 from pygame.locals import *
 
@@ -9,26 +5,31 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 from PIL import Image
-from pywavefront import Wavefront
-
 import math
 
-# =====================================================
+from pywavefront import Wavefront
+
+import pyglet
+
+pyglet.options['shadow_window'] = False
+pyglet.window.Window(visible=False)
+
+# ==================================================
 # CAMERA
-# =====================================================
+# ==================================================
 
 camera_x = 0
-camera_y = 2
-camera_z = -15
+camera_y = 1.5
+camera_z = 0
 
 yaw = 0
 pitch = 0
 
-sensitivity = 0.15
+sensitivity = 0.2
 
-# =====================================================
-# TEXTURA
-# =====================================================
+# ==================================================
+# TEXTURAS
+# ==================================================
 
 def load_texture(filename):
 
@@ -64,9 +65,9 @@ def load_texture(filename):
     return tex_id
 
 
-# =====================================================
+# ==================================================
 # DIREÇÃO DA CAMERA
-# =====================================================
+# ==================================================
 
 def update_camera_direction():
 
@@ -80,9 +81,9 @@ def update_camera_direction():
     return dir_x, dir_y, dir_z
 
 
-# =====================================================
+# ==================================================
 # CUBO
-# =====================================================
+# ==================================================
 
 cube_vertices = [
 
@@ -97,12 +98,11 @@ cube_vertices = [
     (-1, 1, 1)
 ]
 
-
 def draw_textured_cube():
 
     glBegin(GL_QUADS)
 
-    # trás
+    # traseira
     glNormal3f(0,0,-1)
 
     glTexCoord2f(0,0); glVertex3fv(cube_vertices[0])
@@ -110,7 +110,7 @@ def draw_textured_cube():
     glTexCoord2f(1,1); glVertex3fv(cube_vertices[2])
     glTexCoord2f(0,1); glVertex3fv(cube_vertices[3])
 
-    # frente
+    # frontal
     glNormal3f(0,0,1)
 
     glTexCoord2f(0,0); glVertex3fv(cube_vertices[4])
@@ -118,7 +118,7 @@ def draw_textured_cube():
     glTexCoord2f(1,1); glVertex3fv(cube_vertices[6])
     glTexCoord2f(0,1); glVertex3fv(cube_vertices[7])
 
-    # baixo
+    # inferior
     glNormal3f(0,-1,0)
 
     glTexCoord2f(0,0); glVertex3fv(cube_vertices[0])
@@ -126,7 +126,7 @@ def draw_textured_cube():
     glTexCoord2f(1,1); glVertex3fv(cube_vertices[5])
     glTexCoord2f(0,1); glVertex3fv(cube_vertices[4])
 
-    # cima
+    # superior
     glNormal3f(0,1,0)
 
     glTexCoord2f(0,0); glVertex3fv(cube_vertices[3])
@@ -153,67 +153,46 @@ def draw_textured_cube():
     glEnd()
 
 
-# =====================================================
-# OBJ
-# =====================================================
-
-def draw_obj_model(scene, tex_id):
-
-    glBindTexture(GL_TEXTURE_2D, tex_id)
-
-    for mat in scene.materials.values():
-
-        verts = mat.vertices
-
-        count = len(verts) // 8
-
-        array_type = (GLfloat * len(verts))(*verts)
-
-        glEnableClientState(GL_VERTEX_ARRAY)
-        glEnableClientState(GL_NORMAL_ARRAY)
-        glEnableClientState(GL_TEXTURE_COORD_ARRAY)
-
-        glInterleavedArrays(
-            GL_T2F_N3F_V3F,
-            0,
-            array_type
-        )
-
-        glDrawArrays(GL_TRIANGLES, 0, count)
-
-        glDisableClientState(GL_TEXTURE_COORD_ARRAY)
-        glDisableClientState(GL_NORMAL_ARRAY)
-        glDisableClientState(GL_VERTEX_ARRAY)
-
-
-# =====================================================
+# ==================================================
 # OPENGL
-# =====================================================
+# ==================================================
 
 def init_opengl(display):
 
     glEnable(GL_DEPTH_TEST)
+
     glEnable(GL_TEXTURE_2D)
 
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
 
+    glEnable(GL_NORMALIZE)
+
     glEnable(GL_COLOR_MATERIAL)
+
+    glColorMaterial(
+        GL_FRONT_AND_BACK,
+        GL_AMBIENT_AND_DIFFUSE
+    )
+
+    # glLightfv(
+    #     GL_LIGHT0,
+    #     GL_POSITION,
+    #     (0, 30, -5, 1)
+    # )
+
+    # Luz ambiente
 
     glLightModelfv(
         GL_LIGHT_MODEL_AMBIENT,
-        (0.3,0.3,0.3,1)
+        (0.6, 0.6, 0.9, 1.0)
     )
+
+    # Luz difusa
 
     glLightfv(
         GL_LIGHT0,
         GL_DIFFUSE,
-        (1,1,1,1)
-    )
-
-    glLightfv(
-        GL_LIGHT0,
-        GL_SPECULAR,
         (1,1,1,1)
     )
 
@@ -233,7 +212,7 @@ def init_opengl(display):
 
     gluPerspective(
         45,
-        display[0]/display[1],
+        display[0] / display[1],
         0.1,
         200
     )
@@ -241,9 +220,227 @@ def init_opengl(display):
     glMatrixMode(GL_MODELVIEW)
 
 
-# =====================================================
+#==================
+#=======Chão=======
+#==================
+
+def draw_chao(tex_grama):
+
+    glBindTexture(GL_TEXTURE_2D, tex_grama)
+
+    glPushMatrix()
+
+    glTranslatef(0,-2,0)
+
+    glScalef(40,0.1,40)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+    
+
+def draw_paredes(tex_parede):
+
+    glBindTexture(GL_TEXTURE_2D, tex_parede)
+
+    paredes = [
+
+        (0,3,40,0),
+        (0,3,-40,0),
+        (40,3,0,90),
+        (-40,3,0,90)
+
+    ]
+
+    for x,y,z,r in paredes:
+
+        glPushMatrix()
+
+        glTranslatef(x,y,z)
+
+        glRotatef(r,0,1,0)
+
+        glScalef(40,5,0.2)
+
+        draw_textured_cube()
+
+        glPopMatrix()
+
+def draw_casa(tex_parede,tex_madeira):
+    
+    # frente 
+    glBindTexture(GL_TEXTURE_2D, tex_parede)
+
+    glPushMatrix()
+
+    glTranslatef(0,1,25)
+
+    glScalef(5,3,0.2)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+    #tras
+
+    glPushMatrix()
+
+    glTranslatef(0,1,15)
+
+    glScalef(5,3,0.2)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+    #esquerda
+    glPushMatrix()
+
+    glTranslatef(-5,1,20)
+
+    glRotatef(90,0,1,0)
+
+    glScalef(5,3,0.2)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+    #direita
+    glPushMatrix()
+
+    glTranslatef(5,1,20)
+
+    glRotatef(90,0,1,0)
+
+    glScalef(5,3,0.2)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+    #telhado
+    glBindTexture(GL_TEXTURE_2D, tex_madeira)
+
+    glPushMatrix()
+
+    glTranslatef(0,4.5,20)
+
+    glScalef(6,0.3,6)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+#porta
+    glPushMatrix()
+
+    glTranslatef(0,-0.5,14.7)
+
+    glScalef(0.8,1.5,0.1)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+    #janelas
+    glPushMatrix()
+
+    glTranslatef(-2,1,14.7)
+
+    glScalef(0.5,0.5,0.1)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+    glPushMatrix()
+
+    glTranslatef( 2,1,14.7)
+
+    glScalef(0.5,0.5,0.1)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+def draw_cerca(tex_madeira):
+    glBindTexture(GL_TEXTURE_2D, tex_madeira)
+
+    for i in range(-40,40,3):
+
+        glPushMatrix()
+
+        glTranslatef(i,-0.5,0)
+
+        glScalef(0.2,2,0.2)
+
+        draw_textured_cube()
+
+        glPopMatrix()
+
+
+    glPushMatrix()
+
+    glTranslatef(0, 0.3, 0)
+
+    glScalef(40, 0.1, 0.1)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+
+    glPushMatrix()
+
+    glTranslatef(0, 1.0, 0)
+
+    glScalef(40, 0.1, 0.1)
+
+    draw_textured_cube()
+
+    glPopMatrix()
+
+def draw_obj_model(scene, tex_id):
+    glEnable(GL_TEXTURE_2D) # Garante que texturas estão ligadas
+    glBindTexture(GL_TEXTURE_2D, tex_id) # Vincula a textura do modelo
+    for mat in scene.materials.values(): # Percorre os materiais do objeto OBJ
+        verts = mat.vertices  # Pega a lista de dados [x,y,z, nx,ny,nz, u,v]
+        count = len(verts) // 8 # Cada vértice tem 8 valores
+        array_type = (GLfloat * len(verts))(*verts) # Converte para o formato de array do C (usado pelo OpenGL)
+        glEnableClientState(GL_VERTEX_ARRAY) # Habilita o uso de arrays de vértices na GPU
+        glEnableClientState(GL_NORMAL_ARRAY) # Habilita o uso de vetores normais (para luz)
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY) # Habilita o uso de coordenadas de textura
+        # T2F_N3F_V3F indica que o array contém: Texture (2 floats), Normal (3 floats), Vertex (3 floats)
+        glInterleavedArrays(GL_T2F_N3F_V3F, 0, array_type) 
+        glDrawArrays(GL_TRIANGLES, 0, count) # Desenha todos os triângulos do modelo de uma vez
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY) # Desabilita os estados para não afetar outros desenhos
+        glDisableClientState(GL_NORMAL_ARRAY)
+        glDisableClientState(GL_VERTEX_ARRAY)
+
+
+def draw_cat(cat, cat_tex):
+
+    glPushMatrix()
+
+    # posição na fazenda
+    glTranslatef(0, -1, 10)
+
+    # orientação
+    glRotatef(180, 0, 1, 0)
+    glRotatef(-90, 1, 0, 0)
+
+    # tamanho
+    glScalef(0.02, 0.02, 0.02)
+
+    draw_obj_model(cat, cat_tex)
+
+    glPopMatrix()
+
+# ==================================================
 # MAIN
-# =====================================================
+# ==================================================
 
 def main():
 
@@ -255,54 +452,28 @@ def main():
 
     pygame.init()
 
-    display = (1280,720)
+    display = (1280, 720)
 
     pygame.display.set_mode(
         display,
         DOUBLEBUF | OPENGL
     )
 
-    pygame.mouse.set_visible(False)
     pygame.event.set_grab(True)
+    pygame.mouse.set_visible(False)
 
     init_opengl(display)
-
-    # TEXTURAS
 
     tex_grama = load_texture("texturas/grama.jpg")
     tex_parede = load_texture("texturas/parede.jpg")
     tex_madeira = load_texture("texturas/madeira.jpg")
 
-    # OBJS
+    cat_tex = load_texture("OBJS/Cat/Cat_diffuse.jpg")
 
-    cat_scene = Wavefront(
+    cat = Wavefront(
         "OBJS/Cat/Cat.obj",
         collect_faces=True,
         parse=True
-    )
-
-    cat_tex = load_texture(
-        "OBJS/Cat/Cat_diffuse.jpg"
-    )
-
-    tree_scene = Wavefront(
-        "OBJS/Tree/Tree.obj",
-        collect_faces=True,
-        parse=True
-    )
-
-    tree_tex = load_texture(
-        "OBJS/Tree/Tree.jpg"
-    )
-
-    car_scene = Wavefront(
-        "OBJS/Car/Car.obj",
-        collect_faces=True,
-        parse=True
-    )
-
-    car_tex = load_texture(
-        "OBJS/Car/Car.jpg"
     )
 
     clock = pygame.time.Clock()
@@ -323,12 +494,18 @@ def main():
                 if event.key == K_ESCAPE:
                     running = False
 
+        # Mouse
+
         dx, dy = pygame.mouse.get_rel()
 
         yaw += dx * sensitivity
         pitch -= dy * sensitivity
 
+        pitch = max(-89, min(89, pitch))
+
         dir_x, dir_y, dir_z = update_camera_direction()
+
+        # Movimento
 
         keys = pygame.key.get_pressed()
 
@@ -366,145 +543,40 @@ def main():
             camera_y + dir_y,
             camera_z + dir_z,
 
-            0,1,0
+            0,
+            1,
+            0
         )
+
+        # Luz principal
 
         glLightfv(
             GL_LIGHT0,
             GL_POSITION,
-            (0,15,0,1)
+            (0, 15, -5, 1)
         )
 
-        # ========================
+        # ==================================
         # CHÃO
-        # ========================
+        # ==================================
 
-        glBindTexture(GL_TEXTURE_2D, tex_grama)
+        draw_chao(tex_grama)
 
-        glPushMatrix()
-
-        glTranslatef(0,-2,0)
-        glScalef(30,0.1,30)
-
-        draw_textured_cube()
-
-        glPopMatrix()
-
-        # ========================
+        # ==================================
         # PAREDES
-        # ========================
+        # ==================================
 
-        glBindTexture(GL_TEXTURE_2D, tex_parede)
+        draw_paredes(tex_parede)
 
-        paredes = [
-
-            (0,3,30,0),
-            (0,3,-30,0),
-            (30,3,0,90),
-            (-30,3,0,90)
-
-        ]
-
-        for x,y,z,r in paredes:
-
-            glPushMatrix()
-
-            glTranslatef(x,y,z)
-
-            if r:
-                glRotatef(r,0,1,0)
-
-            glScalef(30,5,0.2)
-
-            draw_textured_cube()
-
-            glPopMatrix()
-
-        # ========================
+        draw_casa(tex_parede,tex_madeira)
+        # ==================================
         # CERCA
-        # ========================
+        # ==================================
 
-        glBindTexture(GL_TEXTURE_2D, tex_madeira)
+        draw_cerca(tex_madeira)
 
-        for i in range(-20,21,4):
 
-            glPushMatrix()
-
-            glTranslatef(i,-0.5,10)
-            glScalef(0.2,2,0.2)
-
-            draw_textured_cube()
-
-            glPopMatrix()
-
-        # ========================
-        # GATO
-        # ========================
-
-        glPushMatrix()
-
-        glTranslatef(0,-1,0)
-
-        glRotatef(-90,1,0,0)
-
-        glScalef(
-            0.02,
-            0.02,
-            0.02
-        )
-
-        draw_obj_model(
-            cat_scene,
-            cat_tex
-        )
-
-        glPopMatrix()
-
-        # ========================
-        # ÁRVORE
-        # ========================
-
-        glPushMatrix()
-
-        glTranslatef(-8,-2,5)
-
-        glRotatef(-90,1,0,0)
-
-        glScalef(
-            0.5,
-            0.5,
-            0.5
-        )
-
-        draw_obj_model(
-            tree_scene,
-            tree_tex
-        )
-
-        glPopMatrix()
-
-        # ========================
-        # CARRO
-        # ========================
-
-        glPushMatrix()
-
-        glTranslatef(8,-2,-5)
-
-        glRotatef(180,0,1,0)
-
-        glScalef(
-            0.03,
-            0.03,
-            0.03
-        )
-
-        draw_obj_model(
-            car_scene,
-            car_tex
-        )
-
-        glPopMatrix()
+        draw_cat(cat,cat_tex)
 
         pygame.display.flip()
 
